ffa99d8889c5043896693d91c146078db9983d3f
maratgareev97 <maratgareev97@gmail.com>
2022-11-20 21:34:56 +0300

    Task_3_1_3

---
+++ src/main/webapp/WEB-INF/views/rest/rolesList.js
@@ -0,0 +1,9 @@
+async function rolesList(){
+//     var roles = await fetch("http://localhost:8080/api/employees/roles")
+//     .then((response) => {
+//         const data = response.text();
+//         return data;
+//     })
+
+// console.log(roles + " !!!!")
+}
