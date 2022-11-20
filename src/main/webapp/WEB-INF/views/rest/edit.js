d10a6b0f27d8e9b810ddfafbe5c03d5fd854507b
maratgareev97 <maratgareev97@gmail.com>
2022-11-20 22:35:32 +0300

    Task_3_1_3

--- src/main/webapp/WEB-INF/views/rest/edit.js
+++ src/main/webapp/WEB-INF/views/rest/edit.js
@@ -63,7 +63,7 @@ async function btnClick() {
 }
 
 
-async function aaa() {
+async function aaa(id) {
     var roles_List = await fetch("http://localhost:8080/api/employees/roles")
         .then((response) => {
             const data = response.text();
